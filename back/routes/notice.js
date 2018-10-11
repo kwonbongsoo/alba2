7af240759a674a2322771752789f5e639a303685
bbong <star1231076@gmail.com>
2018-10-12 01:23:07 +0900

    모든 회원에게 푸시 뼈대완성 현재 공지사항을 등록 하는 프로시저 안에서 등록을 완료하면 상점에 승인된 유저들의 토큰을 받아옴

--- back/routes/notice.js
+++ back/routes/notice.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const datasource = require('../config/datasource')
 const noticeDB = require('../db/noticeDB')
 const connection = datasource.getConnection()
+const push = require('../config/push')
 noticeDB.setConnection(connection)
 
 
@@ -26,6 +27,48 @@ router.post('/s_notice_add', function(req, res, next) {
 
   noticeDB.s_notice_add(params, (result) => {
     console.log(result)
+
+
+    ////
+    // var client_token = 'fuO8eKiK_xA:APA91bHtb2q1FXGfMZQTc3NTlkAuvzxH5rIm9-Bac1_FeL3EMFt0OrhbMFntqn1DwBfAYMhEkVI32OVmjAVQVWruObXZ6CSZCLzYIsYpuHs6tQGuJvVe9w-zkuS7btayFmsCYXcNknBy';
+ 
+    // /** 발송할 Push 메시지 내용 */
+    // var push_data = {
+    //     // 수신대상
+    //     to: client_token,
+    //     // App이 실행중이지 않을 때 상태바 알림으로 등록할 내용
+    //     notification: {
+    //         title: "Hello Node",
+    //         body: "Node로 발송하는 Push 메시지 입니다.",
+    //         sound: "default",
+    //         click_action: "FCM_PLUGIN_ACTIVITY",
+    //         icon: "fcm_push_icon"
+    //     },
+    //     // 메시지 중요도
+    //     priority: "high",
+    //     // App 패키지 이름
+    //     restricted_package_name: "human.nature.customerorderapp",
+    //     // App에게 전달할 데이터
+    //     data: {
+    //         num1: 2000,
+    //         num2: 3000
+    //     }
+    // };
+    
+    // /** 아래는 푸시메시지 발송절차 */
+
+    
+    // push.fcm.send(push_data, function(err, response) {
+    //     if (err) {
+    //         console.error('Push메시지 발송에 실패했습니다.');
+    //         console.error(err);
+    //         return;
+    //     }
+    
+    //     console.log('Push메시지가 발송되었습니다.');
+    //     console.log(response);
+    // });
+    // ////
     res.json(result[0])
   }, (error) => {
     res.status(200)
